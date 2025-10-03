0ff87467d384bd1be64532e1683e27f5b2bdd235
Carl Östling <carlostlong96@gmail.com>
2025-10-03 22:02:45 +0200

    Fix Next.js config to resolve Vercel deployment issues

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    outputFileTracingRoot: undefined,
+  },
 };
 
 export default nextConfig;
